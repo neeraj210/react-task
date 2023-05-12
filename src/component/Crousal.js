2685244edae6dbcb688f0229a97f6549b768e495
neeraj210 <119730606+neeraj210@users.noreply.github.com>
2023-05-12 21:43:55 +0530

    completed 4th task

---
+++ src/component/Crousal.js
@@ -0,0 +1,23 @@
+import React,{Component} from "react";
+
+export class Crousal extends Component{
+    render(){
+        return(
+            <div className="container">
+                
+                <div className="text">
+                <center>
+                <h1>We Make</h1>
+                <h1><b>Things Look Goood</b></h1>
+                
+                </center>
+                
+                </div>
+
+                <img src="https://images.pexels.com/photos/3800848/pexels-photo-3800848.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" width={1300} height={600}/>
+
+            </div>
+
+        )
+    }
+}
